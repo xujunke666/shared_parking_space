fde490a19d604465ed49aa5346573d7eb5b481f3
lg123456-git <964764278@qq.com>
2020-06-09 11:32:13 +0800

    再次上传

---
+++ user/src/main/java/com/wn/user/pojo/User.java
@@ -0,0 +1,13 @@
+package com.wn.user.pojo;
+
+/**
+ * @BelongsProject: shared_parking_space
+ * @BelongsPackage: com.wn.user.pojo
+ * @Author: 廖刚
+ * @CreateTime: 2020-06-09 11:31
+ * @Description:
+ */
+public class User {
+
+
+}
